0a4df0f04a5887cf0b2a1ee7f51d93b267846357
hzhj77 <d245522099@163.com>
2015-04-24 22:22:04 +0800

    test commit
    测试上传

--- StepUp/StepItUp/Views/ViewController/Discover/DiscoverViewController.swift
+++ StepUp/StepItUp/Views/ViewController/Discover/DiscoverViewController.swift
@@ -13,6 +13,7 @@ class DiscoverViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.view.backgroundColor = UIColor.whiteColor()
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
