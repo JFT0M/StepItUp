a58b0e7d5dc8322a10458c65fb2bf327320ad436
hzhj77 <d245522099@163.com>
2015-04-28 15:59:08 +0800

    Revert 0f78d9f..d1c9cfc
    This rolls back to commit 0f78d9fb6cbde858adeb34515eca5be87f8f41d6.

---
+++ StepUp/StepItUp/Views/ViewController/Me/FenSiTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FenSiTableViewController.h
+//  StepUp
+//
+//  Created by chenLong on 15/4/25.
+//  Copyright (c) 2015年 JFT0M. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FenSiTableViewController : UITableViewController
+
+@end
---
+++ StepUp/StepItUp/Views/ViewController/Me/MeViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  Page4new
+//
+//  Created by chenLong on 15/4/21.
+//  Copyright (c) 2015年 chenLong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MeViewController : UITableViewController
+
+
+
+
+@end
+
